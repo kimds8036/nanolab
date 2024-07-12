11963f8a5ceb4c9213930bc0297b786b96823110
kimds8036 <kimds8036@naver.com>
2024-07-12 16:13:55 +0900

    fetchNoticeLinks.js 이랑 noticelink.js완성 공지들 한폴더에 제목 카테고리 조회수 등록일자 까지

--- back/models/NoticeLink.js
+++ back/models/NoticeLink.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const noticeLinkSchema = new mongoose.Schema({
+// NoticeLink 스키마 정의
+const NoticeLinkSchema = new Schema({
   category: { type: String, required: true },
-  link: { type: String, required: true },
   title: { type: String, required: true },
-  date: { type: String, required: true }
+  link: { type: String, required: true, unique: true },
+  date: { type: String, required: true },
+  views: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+  type: { type: String, required: true } // 공지 유형 필드 추가
 });
 
-module.exports = mongoose.model('NoticeLink', noticeLinkSchema);
+const NoticeLink = mongoose.model('NoticeLink', NoticeLinkSchema);
+
+module.exports = NoticeLink;
--- back/scripts/createfolder.js
+++ back/scripts/createfolder.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function createCategoryFolder(categoryName) {
-  const dirPath = path.join(__dirname, '../notices', categoryName);
+  const dirPath = path.join(__dirname, '../DB', categoryName);
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath, { recursive: true });
   }
