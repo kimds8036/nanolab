d14378ee53c2840b1e521a08e0bbb93d62d07d67
eunchae0215 <eunchae040215@naver.com>
2024-07-31 16:54:12 +0900

    음

--- front/nanolab/App.js
+++ front/nanolab/App.js
@@ -18,12 +18,8 @@ function App() {
         <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
         <Stack.Screen name="Enter" component={EnterPage} options={{ headerShown: false }} />
         <Stack.Screen name="Main" component={MainPage} options={{ headerShown: false }} />
-<<<<<<< HEAD
-        <Stack.Screen name="Menu" component={Menubar} options={{ headerShown: false }} />
-=======
         <Stack.Screen name="Noticelist" component={Noticelist} options={{ headerShown: false }} />
         <Stack.Screen name="Mypage" component={Mypage} options={{ headerShown: false }} />
->>>>>>> 160aa4da70eb43bd763049ddb21d87b3514e4909
       </Stack.Navigator>
     </NavigationContainer>
   );
