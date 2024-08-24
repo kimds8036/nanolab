e504b2a785ed6e11bfe0e983bee94c483dab9607
kimds8036 <kimds8036@kku.ac.kr>
2024-08-24 22:36:17 +0900

    자동화 완료는 됐는데 오늘날짜기준이라 검증 불가 둘다 완료

--- back/scripts/saveNoticeContent.py
+++ back/scripts/saveNoticeContent.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import pymongo
 from bs4 import BeautifulSoup, Comment
 from pymongo import MongoClient
 from urllib.parse import urljoin
@@ -44,6 +45,40 @@ def get_notice_links(db):
     print(f"Number of notice links found: {len(notice_links_list)}")
     return notice_links_list
 
+def get_today_notice_links(db):
+    today_date_dash = datetime.now().strftime('%Y-%m-%d')
+    today_date_dot = datetime.now().strftime('%Y.%m.%d')
+
+    cursor = db.noticelinks.find({
+        '$or': [
+            {'date': today_date_dash},
+            {'date': today_date_dot}
+        ]
+    })
+    
+    notice_links_list = list(cursor)
+    print(f"Number of today's notice links found: {len(notice_links_list)}")
+    return notice_links_list
+
+def get_existing_importance(db, link):
+    notice_link = db.noticelinks.find_one({'link': link})
+    if notice_link:
+        return notice_link.get('importance', 'general')
+    return 'general'
+
+def update_importance_if_changed(db, notice_link):
+    existing_importance = get_existing_importance(db, notice_link['link'])
+    new_importance = notice_link.get('importance', 'general')
+    
+    if existing_importance != new_importance:
+        db.noticelinks.update_one(
+            {'link': notice_link['link']},
+            {'$set': {'importance': new_importance}}
+        )
+        print(f"Updated importance for {notice_link['link']} from {existing_importance} to {new_importance}")
+    else:
+        print(f"No change in importance for {notice_link['link']}")
+
 # 공지사항 저장 및 업데이트
 def save_or_update_notice(db, category, notice_data):
     try:
@@ -62,8 +97,6 @@ def save_or_update_notice(db, category, notice_data):
         print(f"Notice saved or updated in {collection_name}: {notice_data['title']}")
     except Exception as e:
         print('Error saving or updating notice in category collection:', e)
-
-
 
 categories = [
     {'name': '신문방송학과', 'baseUrl': 'https://masscom.kku.ac.kr', 'type': 'second'},
@@ -102,46 +135,34 @@ categories = [
     {'name': '혁신공유대학', 'baseUrl': 'https://healingbio.kku.ac.kr', 'type': 'second'}
 ]
 
-
-def get_latest_notice_date(db, category):
-    # 카테고리별로 가장 최근에 저장된 공지사항 날짜 가져오기
-    collection_name = f'notices_{category}'
-    collection = db[collection_name]
-    latest_notice = collection.find_one(sort=[("date", pymongo.DESCENDING)])
-    return latest_notice['date'] if latest_notice else '1970-01-01'
-# 공지사항 크롤링 및 저장
 def scrape_and_save_notices():
     db = connect_to_mongodb()
 
-    print("Fetching notice links...")
-    notice_links = get_notice_links(db)
-    print(f"Fetched {len(notice_links)} notice links.")
+    notice_links = get_today_notice_links(db)
+
+    if not notice_links:
+        print("No notices for today. Updating importance only.")
+        # 오늘의 공지사항이 없을 때, 모든 링크에 대해 중요도만 업데이트
+        all_links = get_notice_links(db)
+        for notice_link in all_links:
+            update_importance_if_changed(db, notice_link)
+        return
 
     for notice_link in notice_links:
-        print(f"Processing link: {notice_link['link']}")
-        link = notice_link['link']
-        category = notice_link['category']
-        result = None  # 초기화
-
-        # 카테고리 매칭
-        matching_category = next((c for c in categories if c['name'] == category), None)
-        if not matching_category:
-            print(f'Unrecognized category: {category}')
-            continue
-
-        latest_date = get_latest_notice_date(db, category)
-        notice_date = datetime.strptime(notice_link['date'], "%Y-%m-%d")
-
-        # 이전에 저장된 공지사항 이후의 것만 처리
-        if notice_date <= datetime.strptime(latest_date, "%Y-%m-%d"):
-            print(f"Skipping notice from {notice_date}, as it's not newer than the latest saved date {latest_date}")
-            continue
-
-        start_time = time.time()
+        # 스크래핑 로직 시작
         try:
-            response = requests.get(link, headers=headers)
+            response = requests.get(notice_link['link'], headers=headers)
             response.raise_for_status()
             soup = BeautifulSoup(response.text, 'html.parser')
+
+            # 중요도 가져와서 업데이트
+            update_importance_if_changed(db, notice_link)
+
+            category = notice_link['category']
+            matching_category = next((c for c in categories if c['name'] == category), None)
+            if not matching_category:
+                print(f"Unrecognized category: {category}")
+                continue
 
             if category == '의예과':
                 title_element = soup.select_one('h4 strong')
@@ -154,14 +175,13 @@ def scrape_and_save_notices():
 
                 content_element = soup.select_one('.board-view-con')
                 if content_element:
-                    # 본문 내 모든 링크에 도메인 추가
                     for a_tag in content_element.find_all('a', href=True):
-                        a_tag['href'] = urljoin(link, a_tag['href'])
+                        a_tag['href'] = urljoin(notice_link['link'], a_tag['href'])
 
                     image_urls = []
                     for img in content_element.find_all('img'):
                         img_src = img['src'].strip()
-                        img_url = urljoin(link, img_src)
+                        img_url = urljoin(notice_link['link'], img_src)
                         image_urls.append(img_url)
                         img['src'] = img_url
 
@@ -172,17 +192,16 @@ def scrape_and_save_notices():
                     extracted_text = '내용 없음'
                     image_urls = []
 
-                # 첨부 파일과 일반 링크 구분
                 all_files = []
                 valid_links = []
 
                 for a_tag in soup.find_all('a', href=True):
                     href = a_tag['href'].strip()
                     if "/common/downLoad.do" in href or "/file/fileDownLoad.do" in href:
-                        file_url = urljoin(link, href)
+                        file_url = urljoin(notice_link['link'], href)
                         all_files.append(file_url)
                     else:
-                        valid_links.append(urljoin(link, href))
+                        valid_links.append(urljoin(notice_link['link'], href))
 
                 pdf_url = None
 
@@ -193,15 +212,14 @@ def scrape_and_save_notices():
                     'files': all_files,
                     'extractedText': extracted_text,
                     'images': image_urls,
-                    #'links': valid_links,
                     'pdfUrl': pdf_url,
                     'type': notice_link.get('type', {})
                 }
                 print(f"Processed notice: {result}")
             else:
-                print(f"Necessary elements not found for link: {link}")
+                print(f"Necessary elements not found for link: {notice_link['link']}")
 
-            valid_links = []  # valid_links를 처음에 초기화
+            valid_links = []
 
             if matching_category['type'] == 'first':
                 title = soup.select_one('h4').get_text(strip=True).replace('제목\t :', '').strip() or '제목 없음'
@@ -209,14 +227,13 @@ def scrape_and_save_notices():
                 content_element = soup.select_one('#board_contents')
 
                 all_files = []
-                # 첫 번째 카테고리의 파일 링크 추가
                 for a_tag in soup.select('div.detail_info span.detail_info_after a[href]'):
                     href = a_tag['href'].strip()
                     if "/common/downLoad.do" in href or "/file/fileDownLoad.do" in href:
-                        file_url = urljoin(link, href)
+                        file_url = urljoin(notice_link['link'], href)
                         all_files.append(file_url)
                     else:
-                        valid_links.append(urljoin(link, href))
+                        valid_links.append(urljoin(notice_link['link'], href))
 
             elif matching_category['type'] == 'second':
                 title = soup.select_one('table.grid .subject').get_text(strip=True) or '제목 없음'
@@ -224,48 +241,41 @@ def scrape_and_save_notices():
                 content_element = soup.select_one('#post_view_txt')
 
                 all_files = []
-                # 두 번째 카테고리의 파일 링크 추가
                 for a_tag in soup.select('div.post_add div.addfile a[href]'):
                     href = a_tag['href'].strip()
                     if "/common/downLoad.do" in href or "/file/fileDownLoad.do" in href:
-                        file_url = urljoin(link, href)
+                        file_url = urljoin(notice_link['link'], href)
                         all_files.append(file_url)
                     else:
-                        valid_links.append(urljoin(link, href))
+                        valid_links.append(urljoin(notice_link['link'], href))
+
             image_urls = []
             pdf_url = []
             if content_element:
                 clean_content(soup, content_element)
 
-                # 본문 내 모든 링크에 도메인 추가
                 for a_tag in content_element.find_all('a', href=True):
-                    a_tag['href'] = urljoin(link, a_tag['href'])
-                    valid_links.append(a_tag['href'])  # 본문 내 링크도 valid_links에 추가
+                    a_tag['href'] = urljoin(notice_link['link'], a_tag['href'])
+                    valid_links.append(a_tag['href'])
 
                 pdf_url = None
                 iframe_element = content_element.select_one('iframe')
                 if iframe_element:
-                    # PDF URL 절대 경로로 변환
-                    pdf_url = urljoin(link, iframe_element['src'])
+                    pdf_url = urljoin(notice_link['link'], iframe_element['src'])
                     pdf_embed_code = f'<iframe src="{pdf_url}" width="100%" height="800px"></iframe>'
-                    # 기존 iframe 요소를 새로 만든 임베드 코드로 대체
                     content = content_element.prettify().replace(str(iframe_element), pdf_embed_code)
                 else:
                     content = content_element.prettify()
 
-                # 본문 내 모든 링크와 이미지에 도메인 추가
                 for tag in content_element.find_all(['a', 'img', 'iframe'], src=True):
-                    tag['src'] = urljoin(link, tag['src'])
+                    tag['src'] = urljoin(notice_link['link'], tag['src'])
 
                 for tag in content_element.find_all('a', href=True):
-                    tag['href'] = urljoin(link, tag['href'])
+                    tag['href'] = urljoin(notice_link['link'], tag['href'])
 
-                # prettify를 통해 HTML 문자열로 변환
                 content = content_element.prettify()
-
                 extracted_text = content_element.get_text(strip=True)
 
-                # 결과 확인
                 print("PDF URL:", pdf_url)
                 print("Content with domain:", content)
                 print("Extracted Text:", extracted_text)
@@ -277,14 +287,15 @@ def scrape_and_save_notices():
                 'files': all_files,
                 'extractedText': extracted_text,
                 'images': image_urls,
-                #'links': valid_links,  # 링크를 links 필드에 저장 (이 줄 주석처리 유지)
                 'pdfUrl': pdf_url,
                 'type': notice_link.get('type', {})
             }
 
             print(f"Processed notice: {result}")
 
-
+            # 공지사항 저장
+            if result:
+                save_or_update_notice(db, category, result)
 
         except Exception as e:
             print(f'Error scraping notice content for category {category}:', e)
@@ -292,12 +303,8 @@ def scrape_and_save_notices():
         elapsed_time = time.time() - start_time
         print(f"Time taken to fetch and process page for {category}: {elapsed_time} seconds")
 
-        if result:
-            save_or_update_notice(db, category, result)
-
     db.client.close()
 
-# 스크립트 실행
 if __name__ == "__main__":
     try:
         scrape_and_save_notices()
