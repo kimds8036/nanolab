6d5c41d0a80fd79ab8876edde368a99f705b3ee8
kimds8036 <kimds8036@naver.com>
2024-08-26 05:15:48 +0900

    집가자

--- back/server.js
+++ back/server.js
@@ -18,10 +18,18 @@ const Notice = require('./models/Notice');
 const admin = require('firebase-admin');
 
 // FIREBASE_CONFIG 환경 변수에서 JSON 문자열을 가져와서 파싱
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
-// private_key의 \n 처리
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // 줄바꿈 변환
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
